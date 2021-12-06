3d5a83e4f65323049b14c8f31b3e563bc69bc34e
Frank-Proulx <frankproulx7@gmail.com>
2021-12-06 15:35:00 -0800

    add method for diagonal true having passed tests
    Co-authored-by: Marisa Edgar <marisaedgar1212@gmail.com>

--- lib/queen_attack.rb
+++ lib/queen_attack.rb
@@ -1,7 +1,11 @@
 def queen_attack?(position1, position2)
+  difference_x = position1[0] - position2[0]
+  difference_y = position1[1] - position2[1]
   if position1[0] == position2[0]
     return true
   elsif position1[1] == position2[1]
+    return true
+  elsif (difference_x.abs() == difference_y.abs())
     return true
   else
     false
--- spec/queen_attack_spec.rb
+++ spec/queen_attack_spec.rb
@@ -10,8 +10,12 @@ describe('Array#queen_attack?') do
     expect(queen_attack?([1,1],[1,5])).to(eq(true))
   end
 
-  it('is true if the vertical coordinates are equal fot both positions') do
+  it('is true if the vertical coordinates are equal for both positions') do
     expect(queen_attack?([5,4],[6,4])).to(eq(true))
+  end
+
+  it('is true if the x and y coordinates are increased or decreased by the same amount') do
+    expect(queen_attack?([2,3],[4,1])).to(eq(true))
   end
   
 end
