23eed727e035c92aa14bb463f5aa364eb1a6e9cd
Frank-Proulx <frankproulx7@gmail.com>
2021-12-06 15:01:31 -0800

    add method for general fail as well as horizontal true having passed tests
    Co-authored-by: Marisa Edgar <marisaedgar1212@gmail.com>

---
+++ lib/queen_attack.rb
@@ -0,0 +1,7 @@
+def queen_attack?(position1, position2)
+  if position1[0] == position2[0]
+    return true
+  else
+    false
+  end
+end
--- spec/queen_attack_spec.rb
+++ spec/queen_attack_spec.rb
@@ -1,2 +1,13 @@
 require('rspec')
 require('queen_attack')
+
+describe('Array#queen_attack?') do
+  it('is false if the coordinates are not horizontally, vertically, or diagonally in line with each other') do
+    expect(queen_attack?([1,1],[2, 3])).to(eq(false))
+  end
+
+  it('is true if the horizontal coordinates are equal for both positions') do
+    expect(queen_attack?([1,1],[1,5])).to(eq(true))
+  end
+  
+end
